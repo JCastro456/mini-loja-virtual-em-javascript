3555ba7763a918928bb60b1cf74dcca35b740461
Jackeline Castro <jackelinecastrocardoso@hotmail.com>
2023-07-13 11:37:52 -0300

    mini loja virtual

---
+++ script.js
@@ -0,0 +1,65 @@
+const items = [
+    {
+        id: 0,
+        nome: 'camiseta1',
+        img: './img/camiseta.jpg',
+        quantidade: 0
+    },
+    {
+        id: 1,
+        nome: 'camiseta2',
+        img: './img/camiseta.jpg',
+        quantidade: 0
+    },
+    {
+        id: 2,
+        nome: 'camiseta3',
+        img: './img/camiseta.jpg',
+        quantidade: 0
+    }
+]
+
+inicializarLoja = () => {
+    var containerProdutos = document.getElementById('produtos');
+    items.map((val) => {
+        containerProdutos.innerHTML += `
+          
+          <div class="produto-single">
+              <img src="`+val.img+`"/>
+              <p>`+val.nome+`</p>
+              <a key="`+val.id+`" href="#">Adicionar ao carrinho<a/>
+          </div>
+        
+        `;
+    })    
+}
+
+inicializarLoja();
+
+atualizarCarrinho = () => {
+    var containerCarrinho = document.getElementById('carrinho');
+    containerCarrinho.innerHTML = "";
+    items.map((val) => {
+        if(val.quantidade > 0){
+        containerCarrinho.innerHTML += `
+          <div class="info-single-checkout">
+            <p style=>`+val.nome+`</p>
+            | quantidade: `+val.quantidade+`</p>
+            <hr> 
+          </div>       
+        `;
+        }
+    })    
+}
+
+var links = document.getElementsByTagName('a');
+
+for(var i=0; i < links.length; i++)  {
+    links[i].addEventListener("click",function(){
+        let key = this.getAttribute('key');
+        items[key].quantidade++;
+        atualizarCarrinho();
+        return false;
+    })
+}
+
